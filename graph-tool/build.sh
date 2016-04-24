60daddfeede9c488515557bd1e7a6fbb8d7d17bd
Alexey Strokach <alex.strokach@utoronto.ca>
2016-04-24 17:30:16 -0400

    Update graph-tool and pin boost to 1.59 (omnia)

--- graph-tool/build.sh
+++ graph-tool/build.sh
@@ -10,9 +10,11 @@ export ACLOCAL_FLAGS="-I$PREFIX/share/aclocal"
 export LDFLAGS="-L/usr/lib/gcc/x86_64-linux-gnu/5.3.0 -L$PREFIX/lib"  # -L$STDLIB_DIR"
 export CFLAGS="-I/usr/lib/gcc/x86_64-linux-gnu/5.3.0/include -I$PREFIX/include"
 
-export CC=gcc-5
-export CXX=g++-5
-./configure --enable-openmp --enable-static --prefix=$PREFIX --with-boost="$PREFIX" --with-boost-libdir="$PREFIX/lib" || { cat config.log ; exit 1 ; }  #-I$PREFIX/include/cairo -I$PREFIX/include/cairomm-1.0 -I$PREFIX/include/pycairo -I$PREFIX/include/sigc++-2.0 -I$PREFIX/include/freetype2 -I$PREFIX/lib/cairomm-1.0/include -I$PREFIX/lib/sigc++-2.0/include -I/usr/include -I/usr/include/X11 -I/usr/include/X11/extensions"   
+export PKG_INSTALLDIR="$PREFIX"
+export CC="gcc-5"
+export CXX="g++-5"
+./autogen.sh
+./configure --enable-openmp --enable-static --prefix="$PREFIX" --with-boost="$PREFIX" --with-boost-libdir="$PREFIX/lib" || { cat config.log ; exit 1 ; }  #-I$PREFIX/include/cairo -I$PREFIX/include/cairomm-1.0 -I$PREFIX/include/pycairo -I$PREFIX/include/sigc++-2.0 -I$PREFIX/include/freetype2 -I$PREFIX/lib/cairomm-1.0/include -I$PREFIX/lib/sigc++-2.0/include -I/usr/include -I/usr/include/X11 -I/usr/include/X11/extensions"   
 make
 make check
 make install
