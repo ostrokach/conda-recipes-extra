05788c9daa80ed1edd909da283bc7b7e0249f977
ostrokach <ostrokach@gmail.com>
2016-06-11 17:45:51 -0400

    Update pycairo.

--- pycairo/build.sh
+++ pycairo/build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-ln -s $PREFIX/lib $PREFIX/lib64
-
-export XORG_PREFIX="/usr"
-export PKG_CONFIG_PATH="$PREFIX/share/pkgconfig:$PREFIX/lib/pkgconfig:/usr/lib/pkgconfig"
+# export XORG_PREFIX="/usr"
 export ACLOCAL_FLAGS="-I$PREFIX/share/aclocal"
+export PKG_CONFIG_PATH="$PREFIX/share/pkgconfig:$PREFIX/lib/pkgconfig"
+# export PKG_CONFIG_PATH="$PREFIX/share/pkgconfig:$PREFIX/lib/pkgconfig:/usr/lib/pkgconfig"
 
 export CFLAGS="-I$PREFIX/include -I$PREFIX/include/glib-2.0 -I$PREFIX/include/glib-2.0/gobject -I$PREFIX/include/glib-2.0/glib -I$PREFIX/include/glib-2.0/gio -I$PREFIX/include/gobject-introspection-1.0 -I$PREFIX/lib/glib-2.0/include -I/usr/include -I/usr/include/X11 -I/usr/include/X11/extensions"
-export CPPFLAGS="$CFLAGS"
-export LDFLAGS="-L$PREFIX/lib64 -L$PREFIX/lib -L/usr/lib -L/usr/local/lib"
+export CPPFLAGS="$CFLAGS" 
+export LDFLAGS="-L$PREFIX/lib"
 
 $PYTHON setup.py install
-cp $PREFIX/include/pycairo/py3cairo.h $PREFIX/include/pycairo/pycairo.h
+ln -s $PREFIX/include/pycairo/py3cairo.h $PREFIX/include/pycairo/pycairo.h
 
 unlink $PREFIX/lib64
 
