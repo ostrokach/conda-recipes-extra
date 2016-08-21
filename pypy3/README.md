03e8b6ae31bce1bab435852986b9516c01af74b9
ostrokach <ostrokach@gmail.com>
2016-08-20 20:14:34 -0400

    Add pypy3 recipe.

---
+++ pypy3/README.md
@@ -0,0 +1,4 @@
+# PYPY 3.3
+
+On my benchmarks, PyPy 3.3 runs 5x times slower than PyPy 2.7.10...
+
---
+++ pypy3/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/lib
+mkdir -p $PREFIX/include
+mkdir -p $PREFIX/lib_pypy
+mkdir -p $PREFIX/lib-python
+
+mv $SRC_DIR/bin/pypy* $PREFIX/bin/
+mv $SRC_DIR/bin/virtualenv-pypy $PREFIX/bin/
+mv $SRC_DIR/bin/libpypy-c.so $PREFIX/lib/
+
+mv $SRC_DIR/include/* $PREFIX/include/
+mv $SRC_DIR/lib_pypy/* $PREFIX/lib_pypy/
+mv $SRC_DIR/lib-python/* $PREFIX/lib-python/
+mv $SRC_DIR/virtualenv_support/ $PREFIX/
+
